e2d6088408bb0ef0ba765b7965e42ec8c1abb652
horizonhui <39830775+horizonhui@users.noreply.github.com>
2019-06-13 11:27:44 +0800

    ch (#8)
    增加除法

--- main.cpp
+++ main.cpp
@@ -7,3 +7,11 @@ int main()
     cout << "Hello world!" << endl;
     return 0;
 }
+int divFun(int a, int b){
+    if(b == 0){
+        cout<<"error"<<endl;
+        return 0;
+    }
+    else
+        return a/b;
+}
