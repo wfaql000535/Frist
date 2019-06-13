aec14157975bf7310e621817028e39219fda21fa
GFuDong <51467550+GFuDong@users.noreply.github.com>
2019-06-13 11:25:43 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 
 using namespace std;
 
+int plus(int a,int b)
+{
+  return a+b;
+}
 int main()
 {
     cout << "Hello world!" << endl;
     return 0;
 }
-int plus(int a,int b)
-{
-  return a+b;
-}
+
